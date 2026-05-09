6a6099e43cbed1e1b7afb94c8e9987c2155b3716
Deurff <jalvgado@gmail.com>
2026-05-09 13:10:56 +0200

    Nuevos cambios

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'El D.O.C desarrolla';
-export const SITE_DESCRIPTION = 'Construyendo cosas con Claude sin saber programar';
+export const SITE_TITLE = 'El D.o.C desarrolla';
+export const SITE_DESCRIPTION = 'Construyendo con IA sin saber programar';
